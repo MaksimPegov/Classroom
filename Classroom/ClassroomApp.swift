b174ae285bdc0eff13ae7d541f36d68e42156da8
Maksim Pegov <maksim.peg@gmail.com>
2022-10-06 10:03:58 +0200

    Initial Commit

---
+++ Classroom/ClassroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassroomApp.swift
+//  Classroom
+//
+//  Created by Maksim Pegov on 06/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ClassroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
